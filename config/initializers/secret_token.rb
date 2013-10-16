8ec1fdac51f084cd15e1f8439f611e9bdb43a1be
Piotr Mąsior <pmasior@railsutils.com>
2013-10-16 11:02:57 +0200

    initial commit for RAILS4 application structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularRails::Application.config.secret_key_base = '0f0872a1b022e9bf5f09076ff6f88ca330abe33d169277b04c10a7bde02b0d770a684cb85df9b57fcbba0a1c94f3a0766bf3517718b5093e152b09e97b97c4a9'
