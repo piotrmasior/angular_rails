f5dd111a9c039ec987e6184782b23f2ccec38283
Piotr Mąsior <pmasior@railsutils.com>
2013-10-31 12:06:17 +0100

    checking if angular is working

--- ngapp/app/scripts/controllers/main.js
+++ ngapp/app/scripts/controllers/main.js
@@ -2,14 +2,29 @@
 
 var app = angular.module('arailsApp')
   .controller('MainCtrl', function ($scope) {
-
+    $scope.someFun = function() {
+       console.log("Some funny function");
+    }
   });
 
 
-app.directive("alert", function() {
-    return function(scope, element) {
-        $(element).bind("mouseenter", function () {
-            console.log("Both jquery and no jquery style wokiing");
+//camel cased is not best way
+app.directive("enter", function() {
+    return function(scope, element, attrs) {
+        element.bind("mouseenter", function () {
+            scope.$apply(attrs.enter)
         });
+    }
+});
+
+
+app.directive("supcio", function() {
+    return {
+        restrict: "E",
+        link: function(scope, element) {
+            element.bind("mouseenter", function() {
+                console.log("Wathcout on attrs: ");
+            })
+        }
     }
 });
